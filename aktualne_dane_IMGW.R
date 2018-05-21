6594171852cc47664d25a7cbb582d1a6787e2b93
Mariusz Kupczyk <34220053+mariusz11363@users.noreply.github.com>
2018-05-21 19:46:01 +0200

    Update aktualne_dane_IMGW.R
    Poprawiona wersja ze strefą czasową i filtrowaniem danych.

--- aktualne_dane_IMGW.R
+++ aktualne_dane_IMGW.R
@@ -1,4 +1,4 @@
-IMGW_aktualne <- function(){
+IMGW_aktualne <- function(filtrowanie=T, strefa_czasowa=-2){
   library("dplyr")
   library("RCurl")
   
@@ -24,41 +24,19 @@ IMGW_aktualne <- function(){
   IMGW[,3] <- as.POSIXct(strptime(IMGW[,3], "%Y-%m-%d"), tz = "UTC")
   
   
-  miasta <- read.csv("http://endemit.pl/wspolrzedne.txt", sep=";", header = T)
-  colnames(miasta) <- c("Nazwa_stacji", "x", "y")
   
-  IMGW <- merge(IMGW, miasta, "Nazwa_stacji")
-  IMGW <- IMGW[,c(1:12)]
+ #filtr danych jednej godziny 
   
-  IMGW$xx <- substr(IMGW$x, 4,5)
-  IMGW$yy <- substr(IMGW$y, 4,5)
-  
-  IMGW$x <- substr(IMGW$x, 1,2)
-  IMGW$y <- substr(IMGW$y, 1,2)
-  
-  IMGW$xx <- round(as.numeric(IMGW$xx)/60, 2)
-  IMGW$yy <- round(as.numeric(IMGW$yy)/60, 2)
-  
-  IMGW$x <- as.numeric(IMGW$x)+IMGW$xx
-  IMGW$y <- as.numeric(IMGW$y)+IMGW$yy
-  
-  IMGW <- IMGW[,c(1,2,11,12,3:10)]
-  df = data.frame(t(c("UKR",999999,24.50,48.75,as.character(IMGW$Data[1]),IMGW$Godzina[1],c(round((IMGW$Temp[24]+IMGW$Temp[40])/2,1)),0,0,0,0,0)))
-  colnames(df) <- colnames(IMGW)
-  
-  df1 <- t(c("DEU",0000000,14.00,55.00,as.character(IMGW$Data[1]),IMGW$Godzina[1],c(round((IMGW$Temp[5]+IMGW$Temp[52])/2,1)),0,0,0,0,0)) 
-  colnames(df1) <- colnames(IMGW)
-  
-  IMGW <- rbind(IMGW,df, df1)
-  IMGW$x <- as.numeric(IMGW$x)
-  IMGW$y <- as.numeric(IMGW$y)
-  IMGW$Temp <- as.numeric(IMGW$Temp)
-  
-  IMGW <- IMGW[order(IMGW$y),]
-  IMGW <- IMGW[order(IMGW$x),]
-  
+  if(filtrowanie==T){
   hour <- Sys.time()
   hour <- substr(hour, 12,13)%>%as.numeric()
   IMGW%>%
-    filter(Godzina == hour)
+    filter(Godzina == hour+strefa_czasowa)
+  }else{
+    IMGW
+  }
+  
 }
+
+
+IMGW <- IMGW_aktualne()
